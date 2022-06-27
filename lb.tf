0220fdf0829b593043081ce42e80a6d1d6accd0a
ChaitanyaChandra <chay@outlook.in>
2022-06-27 17:31:57 +0530

    automated commit #831

---
+++ lb.tf
@@ -0,0 +1,11 @@
+resource "aws_lb_target_group" "target_group" {
+  name     = "internal-target-group"
+  port     = var.port
+  protocol = var.protocol
+  vpc_id   = var.vpc_id
+}
+
+# resource "aws_lb_target_group_attachment" "attach_instance" {
+#   target_group_arn = aws_lb_target_group.instance.arn
+#   target_id        = aws_instance.vm[count.index].id
+# }
