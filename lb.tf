f4eb601a00207b24510b8f977007b70c465d581e
ChaitanyaChandra <chay@outlook.in>
2022-06-27 19:47:12 +0530

    automated commit #854

--- lb.tf
+++ lb.tf
@@ -24,3 +24,14 @@ resource "aws_lb_target_group_attachment" "attach_instance" {
   target_group_arn = aws_lb_target_group.target_group.arn
   target_id        = aws_instance.vm.id
 }
+
+resource "aws_lb_listener" "front_end" {
+  load_balancer_arn = aws_lb.application_elb.arn
+  port              = var.port
+  protocol          = "HTTP"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.instance.arn
+  }
+}
