517633d7ee0192dfb8cd17411592af09522b3785
ChaitanyaChandra <chay@outlook.in>
2022-06-27 21:54:02 +0530

    automated commit #893

--- outputs.tf
+++ outputs.tf
@@ -1,4 +1,4 @@
 output "target_group_arn" {
-  value       = aws_lb_target_group_attachment.attach_instance.id
+  value       = aws_lb_target_group.target_group.arn
   description = "target group arn for elb module"
 }
